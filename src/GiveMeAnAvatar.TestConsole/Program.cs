469ae04c49001de619fcaf8845536d311cb35ec6
Clyde D'Souza <clyd94@gmail.com>
2020-12-20 15:07:10 +1300

    Update test console program output

--- src/GiveMeAnAvatar.TestConsole/Program.cs
+++ src/GiveMeAnAvatar.TestConsole/Program.cs
@@ -6,9 +6,22 @@ namespace GiveMeAnAvatar.TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GiveMeAnAvatar.GetAvatarURL());
+            const string packageName = @"
+  _____ _             __  __                                        _             
+  / ____(_)           |  \/  |                        /\            | |            
+ | |  __ ___   _____  | \  / | ___    __ _ _ __      /  \__   ____ _| |_ __ _ _ __ 
+ | | |_ | \ \ / / _ \ | |\/| |/ _ \  / _` | '_ \    / /\ \ \ / / _` | __/ _` | '__|
+ | |__| | |\ V |  __/ | |  | |  __/ | (_| | | | |  / ____ \ V | (_| | || (_| | |   
+  \_____|_| \_/ \___| |_|  |_|\___|  \__,_|_| |_| /_/    \_\_/ \__,_|\__\__,_|_|   
+                                                                                   
+                                                                                   
+            ";
+            
+            Console.WriteLine(packageName); 
+            Console.WriteLine($"Avatar URL generated without passing any settings: {GiveMeAnAvatar.GetAvatarURL()}");
             var settings = new AvatarSettings() { Name = "Axel Drew", Size = 124 };
-            Console.WriteLine(GiveMeAnAvatar.GetAvatarURL(settings));
+            Console.WriteLine($"Avatar URL generated after passing settings: {GiveMeAnAvatar.GetAvatarURL(settings)}");
+            Console.ReadLine();
         }
     }
 }
